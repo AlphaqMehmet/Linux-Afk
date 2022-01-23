077c1221e8d1bd8a09edc735b7703a8af0ca2ad8
AlphaqMehmet <98256509+AlphaqMehmet@users.noreply.github.com>
2022-01-23 13:26:20 +0300

    Add files via upload

---
+++ baslat.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+HesapAdi="badboy251AFK"
+
+while true ; do
+echo "Başlatmayı engellemek için CTRL + C yapın."
+echo "MC-Diyari 3 saniye içinde açılıyor..."
+sleep 1
+echo "MC-Diyari 2 saniye içinde açılıyor..."
+sleep 1
+echo "MC-Diyari 1 saniye içinde açılıyor..."
+sleep 1
+
+/usr/bin/mono MinecraftClient.exe $HesapAdi -
+done
